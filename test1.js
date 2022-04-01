3340fdfaa80b288ebb34ea73fa01033df2547a49
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-04-01 09:48:59 -0700

    first commit

---
+++ test1.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+
+let peer;
+document.querySelector(".set-id").addEventListener('click', ()=>{
+    let myid = document.querySelector(".id-in").value
+    peer = new Peer(myid, {debug: 3,
+        port: 443,})
+
+})
+
+
+let conn = document.querySelector(".call").addEventListener('click', ()=>{
+    conn = peer.connect(document.querySelector(".id-out").value);
+    document.querySelector(".send").addEventListener('click', ()=>{
+        console.log("a")
+        let msg = document.querySelector(".chat").value
+        conn.send(msg);
+    })
+
+})
+
+
+
+
+
