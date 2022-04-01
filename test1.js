117bcfb3c45fa9ea8dd4241a7a6c49d4856a64f5
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-04-01 10:35:48 -0700

    update

--- test1.js
+++ test1.js
@@ -5,7 +5,9 @@ let peer;
 document.querySelector(".set-id").addEventListener('click', ()=>{
     let myid = document.querySelector(".id-in").value
     peer = new Peer(myid, {debug: 3,
-        port: 443,})
+        host: '9000-peers-peerjsserver-m7p3neex58r.ws-eu38.gitpod.io/myapp',
+        port: 9000,
+        path: '/myapp'})
 
 })
 
