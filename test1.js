a5559dfcd5271df5e51f7ce60315b1a63da3c47a
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-04-04 09:34:10 -0700

    conseguido conexion

--- test1.js
+++ test1.js
@@ -1,28 +1,7 @@
 ﻿'use strict';
 
 
-let peer;
-document.querySelector(".set-id").addEventListener('click', ()=>{
-    let myid = document.querySelector(".id-in").value
-    peer = new Peer(myid, {debug: 3,
-        host: '9000-peers-peerjsserver-m7p3neex58r.ws-eu38.gitpod.io/myapp',
-        port: 9000,
-        path: '/myapp'})
-
-})
-
-
-let conn = document.querySelector(".call").addEventListener('click', ()=>{
-    conn = peer.connect(document.querySelector(".id-out").value);
-    document.querySelector(".send").addEventListener('click', ()=>{
-        console.log("a")
-        let msg = document.querySelector(".chat").value
-        conn.send(msg);
-    })
-
-})
-
-
-
-
+chatp2p( new Peer(document.querySelector(".id-in").value, {debug: 3,
+    host: '0.peerjs.com',
+    port: 443}))
 
