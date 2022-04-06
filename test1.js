cccef595e1835f34e354724018d15d982c6c66a1
Eduardo Moraton <eduardo.moraton@iesdoctorbalmis.com>
2022-04-06 19:14:56 +0200

    mensajes funcionan

--- test1.js
+++ test1.js
@@ -1,28 +1,43 @@
 ﻿'use strict';
 
+let msgBox = document.querySelector(".msg-box");
 
-let peer = new Peer({debug: 1,
+var peer = new Peer(prompt("Introduce un ID"), {debug: 1,
     host: '0.peerjs.com',
     port: 443})
-let conn;
+var conn;
 
 document.querySelector(".send").addEventListener("click", ()=>{
     console.log("enviando mensaje")
-    conn.send("hola")
+    console.log(conn)
+    conn.send(document.querySelector(".msg").value)
+    let msgblob = document.createElement("p")
+    msgblob.textContent = document.querySelector(".msg").value
+    msgBox.appendChild(msgblob)
 })
 
 document.querySelector(".call").addEventListener("click", ()=>{
     console.log("llamando")
     conn = peer.connect(document.querySelector(".id-out").value)
+    conn.on("data", (data)=>{
+        let msgblob = document.createElement("p")
+        msgblob.textContent = data
+        msgBox.appendChild(msgblob)
+    })
 })
 
 peer.on('open', function(id) {
 	console.log('My peer ID is: ' + id);
 });
 
-peer.on("connection", (conn)=>{
+peer.on("connection", (conection)=>{
     console.log("connected")
-    conn.on("data", (data)=>console.log(data))
+    conn = conection
+    conn.on("data", (data)=>{
+        let msgblob = document.createElement("p")
+        msgblob.textContent = data
+        msgBox.appendChild(msgblob)
+    })
 })
 
 
