3b267ee917facab811c9b56fe1bb14c3e8b18d04
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-04-06 11:34:49 -0700

    update for test

--- test1.js
+++ test1.js
@@ -1,7 +1,19 @@
 ﻿'use strict';
 
 
-chatp2p( new Peer(document.querySelector(".id-in").value, {debug: 3,
-    host: '0.peerjs.com',
-    port: 443}))
+let peer
+let conn;
+document.querySelector(".set-id").addEventListener("click", () =>{
+    peer = new Peer(document.querySelector(".id-in").value, {debug: 3,
+        host: '0.peerjs.com',
+        port: 443})
+})
+document.querySelector(".call").addEventListener("click", ()=>{
+    conn = peer.connect(document.querySelector(".id-out").value)
+})
+
+
+setInterval(()=>console.log(peer), 1000)
+setInterval(()=>console.log(conn), 1000)
+
 
